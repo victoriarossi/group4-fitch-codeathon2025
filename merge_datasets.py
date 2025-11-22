b08c9b02607188df4667f98fe8163ae199665a12
victoriarossi <vross3@uic.edu>
2025-11-22 16:48:40 -0600

    merged_complete

--- merge_datasets.py
+++ merge_datasets.py
@@ -14,17 +14,27 @@ import pandas as pd
 # merged.to_csv("data/merged_dataset.csv", index=False)
 # print("Merged dataset saved to data/merged_dataset.csv")
 
-df = pd.read_csv("data/merged_dataset_cleaned.csv")
+df1 = pd.read_csv("data/merged_dataset.csv")
 
 # Remove rows that have missing columns region_id until target_scope_2
-df = df[df.loc[:, "region_code":"target_scope_2"].notnull().all(axis=1)]
+# df = df[df.loc[:, "region_code":"target_scope_2"].notnull().all(axis=1)]
 
-df.to_csv("data/merged_dataset.csv", index=False)
+df2 = pd.read_csv("data/merged_dataset_imputed_activity.csv")
+df3 = pd.read_csv("data/merged_dataset_imputed_sdg.csv")
+
+cols_to_merge = ['activity_type','activity_code','env_score_adjustment','env_score_adjustment_capped']
+
+df1[cols_to_merge] = df2[cols_to_merge].values
+
+cols_to_merge = ['sdg_id', 'sdg_name']
+
+df1[cols_to_merge] = df3[cols_to_merge].values
+
+df1.to_csv("data/merged_dataset_complete.csv", index=False)
 # print("Cleaned merged dataset saved to data/merged_dataset_cleaned.csv")
 
-
 print("Missing values per column in merged dataset:")
-missing_counts = df.isnull().sum()
+missing_counts = df1.isnull().sum()
 for col, count in missing_counts.items():
     print(f"{col}: {count}")
 
